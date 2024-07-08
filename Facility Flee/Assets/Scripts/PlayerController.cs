cfbb10e023be4b5ff02ae6feca77585d17d0e0f2
BigRedFruitTree <blaine.dooley07@gmail.com>
2024-07-08 08:16:11 -0700

    Second Quarter
    Changes movement breifly

--- Facility Flee/Assets/Scripts/PlayerController.cs
+++ Facility Flee/Assets/Scripts/PlayerController.cs
@@ -20,8 +20,6 @@ public class PlayerController : MonoBehaviour
     Vector3 moveDirection = Vector3.zero;
     float rotationX = 0;
 
-    public bool canMove = true;
-
     CharacterController characterController;
 
 
@@ -39,52 +37,41 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        #region Handles Movement
+        
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
 
-        //Run Button
 
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        float curSpeedX = canMove ? (isRunning ? dashSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
-        float curSpeedY = canMove ? (isRunning ? dashSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
-        float movementDirectionY = moveDirection.y;
-        moveDirection = (forward * curSpeedX * 10) + (right * curSpeedY * 10);
-        #endregion
+        
 
 
-
+        //Dash
         if (Input.GetKey(KeyCode.E) && characterController.isGrounded)
         {
-            playerRb.AddForce(playerCamera.transform.forward * dashSpeed * Time.deltaTime, ForceMode.Impulse);
+            playerRb.AddForce(Camera.main.transform.forward * dashSpeed * Time.deltaTime, ForceMode.Impulse);
         }
 
 
-        #region Handles Jumping
-        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+        
+        if (Input.GetButton("Jump") && characterController.isGrounded)
         {
             moveDirection.y = jumpPower;
         }
-        else
-        {
-            moveDirection.y = movementDirectionY;
-        }
+
         
         if (!characterController.isGrounded)
         {
             moveDirection.y -= gravity * Time.deltaTime;
         }
-        #endregion
-        #region Handles Rotation
+        
+        
         characterController.Move(moveDirection * Time.deltaTime);
 
-        if (canMove)
-        {
-            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
-            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
-            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
-        }
-        #endregion
+        rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+        rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+
+        
     }
 }
